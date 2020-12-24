99fe0a6f20c51a2196bda12ae23c678c551747fe
Suho Kwon <34533493+Knix008@users.noreply.github.com>
2020-12-24 16:16:15 +0900

    Change migration script

--- migration/4_deploy_contracts.js
+++ migration/4_deploy_contracts.js
@@ -1,12 +1,20 @@
-const METoken = artifacts.require("METoken");
-const METFaucet = artifacts.require("METFaucet");
+var METoken = artifacts.require("METoken");
+var METFaucet = artifacts.require("METFaucet");
 
-module.exports = function(deployer) {
-    deployer.deploy(METoken).then(async function(){
-        let accounts = await web3.eth.getAccounts();
-        let owner = accounts[0];
+//module.exports = function(deployer) {
+//    deployer.deploy(METoken).then(async function(){
+//        let accounts = await web3.eth.getAccounts();
+//        let owner = accounts[0];
 
         //console.log(owner);
-        return deployer.deploy(METFaucet, METoken.address, owner);
-    });
+//        return deployer.deploy(METFaucet, METoken.address, owner);
+//    });
+//}
+module.exports = (deployer) => {
+  deployer.then(async() => {
+    await deployer.deploy(METoken);
+    let accounts = await web3.eth.getAccounts();
+    let owner = accounts[0];
+    await deployer.deploy(METFaucet, METoken.address, owner);
+  })
 }
