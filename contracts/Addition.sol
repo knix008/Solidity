d5227bfe9caf31db8bacb7369f4dcaafb7763d28
Suho Kwon <34533493+Knix008@users.noreply.github.com>
2021-04-16 13:17:02 +0900

    Simple addition

---
+++ contracts/Addition.sol
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: UNLICENSED */
+pragma solidity ^0.8.3;
+
+contract Addition {
+    uint8 x;
+
+    function addx(uint8 y, uint8 z) public {
+        x = y + z;
+    }
+
+    function retrievex() public view returns (uint8) {
+        return x;
+    }
+}
