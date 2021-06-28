6abec10e952f51642c740c7271b6965dedf992f8
Suho Kwon <34533493+Knix008@users.noreply.github.com>
2021-06-28 17:18:51 +0900

    Update startgeth.sh

--- startgeth.sh
+++ startgeth.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-geth --datadir ~/etherprivate --networkid 786 --allow-insecure-unlock --http --http.port 8545 --http.api 'admin,web3,eth,net,debug,personal' --http.corsdomain '*' --nodiscover --identity 'shkwon'
+geth --datadir ~/ethereum init ~/ethereum/genesis.json
+geth --datadir ~/ethereum --nodiscover --allow-insecure-unlock --http --http.port 8545 --http.addr '0.0.0.0' --http.corsdomain '*' --http.api 'admin,eth,debug,miner,net,txpool,personal,web3' --port 30303
